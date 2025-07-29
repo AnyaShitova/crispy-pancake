513b2c06faf47c0ac4d1ccf358f26d78fcc34ce5
Shitova Anna <shitova.anna@ural.rt.ru>
2025-07-29 21:35:26 +0300

    коммит

--- TV.java
+++ TV.java
@@ -2,47 +2,56 @@ public class TV {
     private String brand;
     private int size;
     private boolean isOn;
-    public TV (String brand, int size){
-       this.brand = brand;
-       this.size = size;
-       this.isOn = false;
+
+    public TV(String brand, int size) {
+        this.brand = brand;
+        this.size = size;
+        this.isOn = false;
     }
-    public String getBrand(){
+
+    public String getBrand() {
         return brand;
     }
-    public void setBrand(String brand){
+
+    public void setBrand(String brand) {
         this.brand = brand;
     }
-    public int getSize(){
+
+    public int getSize() {
         return size;
     }
-    public void setSize(int size){
+
+    public void setSize(int size) {
         this.size = size;
     }
-    public boolean isOn(){
+
+    public boolean isOn() {
         return isOn;
     }
-    public void turnOn(){
-        if(!isOn){
+
+    public void turnOn() {
+        if (!isOn) {
             isOn = true;
             System.out.println("Телевизор " + brand + " включен.");
         } else {
             System.out.println("Телевизор " + brand + " уже включен.");
         }
     }
-    public void turnOff(){
-        if (isOn){
+
+    public void turnOff() {
+        if (isOn) {
             isOn = false;
             System.out.println("Телевизор " + brand + " выключен.");
         } else {
             System.out.println("Телевизор " + brand + " уже выключен.");
         }
     }
-    public void changeChannel(int channel){
-        if (isOn){
+
+    public void changeChannel(int channel) {
+        if (isOn) {
             System.out.println("Телевизор " + brand + " переключен на канал " + channel);
         } else {
             System.out.println("Телевизор " + brand + " выключен. Включите его для смены канала.");
         }
-        }
     }
+}
